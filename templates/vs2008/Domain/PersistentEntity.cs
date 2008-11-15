2efa963dee0850091e14bae384306f8f50f45dda
benjamin.hart <benjamin.hart@d53acd0e-b2fa-11dd-8f72-659bbef264a5>
2008-11-15 17:29:30 +0000

    updated templates

--- templates/vs2008/Domain/PersistentEntity.cs
+++ templates/vs2008/Domain/PersistentEntity.cs
@@ -1,20 +1,20 @@
-﻿namespace Domain
+﻿using System;
+
+namespace $safeprojectname$
 {
     /// <summary>
     /// Base class for entities. Has an integer Id, which is used both for object identity and generating hash codes.
     /// </summary>
     public abstract class PersistentEntity
     {
-        virtual public int Id { get; set; }
+        private int? _oldHashCode;
+
+        virtual public Guid Id { get; set; }
         
         public override bool Equals(object obj)
         {
-            PersistentEntity entity = obj as PersistentEntity;
-            return Equals(entity);
-        }
+            PersistentEntity other = obj as PersistentEntity;
 
-        public virtual bool Equals(PersistentEntity other)
-        {
             if (this == other)
             {
                 return true;
@@ -24,13 +24,32 @@
             {
                 return false;
             }
+            //Transient entities will both have the same id, and as such we must check for reference equality.
+            if (this.Id == Guid.Empty || other.Id == Guid.Empty)
+            {
+                return ReferenceEquals(this, other);
+            }
 
             return this.Id == other.Id;
         }
 
         public override int GetHashCode()
         {
-            return Id;
+            // Once we have a hash code we'll never change it
+            if (_oldHashCode.HasValue)
+            {
+                return _oldHashCode.Value;
+            }
+
+            // When this instance is transient, we use the base GetHashCode()
+            // and remember it, so an instance can NEVER change its hash code.
+            if (Id == Guid.Empty)
+            {
+                _oldHashCode = base.GetHashCode();
+                return _oldHashCode.Value;
+            }
+
+            return Id.GetHashCode();
         }
     }
 }
---
+++ templates/vs2008/Domain/Student.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace $safeprojectname$
+{
+    public class Student : PersistentEntity
+    {
+        public Student()
+        {
+            Courses =  new List<Course>();
+        }
+
+        public virtual string Name { get; set; }
+        public virtual IList<Course> Courses { get; private set; }
+    }
+}
