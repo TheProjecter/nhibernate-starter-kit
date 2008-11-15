8abc6b26a3d21a829cb93169d45627bc75b8ef2c
benjamin.hart <benjamin.hart@d53acd0e-b2fa-11dd-8f72-659bbef264a5>
2008-11-15 11:34:30 +0000

    solution and projects

---
+++ src/vs2008/Domain/PersistentEntity.cs
@@ -0,0 +1,36 @@
+﻿namespace Domain
+{
+    /// <summary>
+    /// Base class for entities. Has an integer Id, which is used both for object identity and generating hash codes.
+    /// </summary>
+    public abstract class PersistentEntity
+    {
+        virtual public int Id { get; set; }
+        
+        public override bool Equals(object obj)
+        {
+            PersistentEntity entity = obj as PersistentEntity;
+            return Equals(entity);
+        }
+
+        public virtual bool Equals(PersistentEntity other)
+        {
+            if (this == other)
+            {
+                return true;
+            }
+
+            if (other == null)
+            {
+                return false;
+            }
+
+            return this.Id == other.Id;
+        }
+
+        public override int GetHashCode()
+        {
+            return Id;
+        }
+    }
+}
